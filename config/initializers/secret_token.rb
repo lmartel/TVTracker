0bfc3e541af907e2da4a196d7f847743a30b81e3
Leo <github.com/lmartel>
2013-05-23 20:32:01 -0700

    fixed secret token vulnerability (moved to ENV and changed it)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TVTrackerPrototype::Application.config.secret_token = '57172445db92cf338bb9f1b88c38212cea9f9136d6dca2992bff6b2d6497d168c49703e4718c676b6e8eed704126db952061eb32f3817d05a889e2ff3b3fda16'
+TVTrackerPrototype::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
