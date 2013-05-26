26887c347797ddb71b07c21a370ced9305ead862
Leo <github.com/lmartel>
2013-05-26 06:01:11 -0700

    old session protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound # Protects against old sessions after a db reset
   end
 
   helper_method :current_user
