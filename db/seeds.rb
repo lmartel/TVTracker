b8b4fc8bba48f4a6259b707fb2bea9f81569b95b
Leo <github.com/lmartel>
2013-05-26 17:20:19 -0700

    fixed formatting of table for large episode names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 names = "The Voice, American Idol, Mad Men, Dancing with the Stars, Scandal, NCIS, Game of Thrones, Grey's Anatomy, Person of Interest, The Big Bang Theory, Two and a Half Men, Chicago Fire, The Bachelor, The Following, Revolution, The Carrie Diaries, Vegas, Nashville, Arrow, The Amazing Race , Elementary, Hannibal, Modern Family, Parks and Recreation, Criminal Minds, Bones, Revenge, New Girl, Suits, Breaking Bad, True Blood, Once Upon a Time, Glee, Keeping Up with the Kardashians, 2 Broke Girls, Justified, Castle, House of Cards, How I Met Your Mother, 30 Rock, Community, The Vampire Diaries, One Life to Live, Dexter, The Walking Dead, Pretty Little Liars, Nikita, Bad Girls Club, CSI: Crime Scene Investigation, Days of Our Lives, Secret Life of the American Teenager, The Americans, All My Children, Archer, Girls, House of Lies, Shameless, Smash, Masterpiece, Luther, 90210, Cougar Town, Homeland, Bunheads, Psych, Necessary Roughness, White Collar, Alphas, Being Human, Anger Management, Dallas, Happy Endings, The Mindy Project, The Newsroom, Nurse Jackie, Portlandia, Project Runway, Saturday Night Live, Jimmy Kimmel Live, Colbert Report, The Daily Show With Jon Stewart, Spartacus, Southland, Hawaii Five-0, Grimm, iCarly, Supernatural, Californication, Family Guy, The Young and the Restless, Law &amp; Order: Special Victims Unit, American Horror Story: Asylum, The Office, Fringe, The Mentalist, The Good Wife, Private Practice, Sons of Anarchy, Dr. Oz Show, General Hospital, "
 names = names.split(", ")
+MULTITHREADED = true
 
-names.each do |name|
+def pull_name(name)
   begin
     s = Show.find_by_name(name)
     s = Show.create(name: name) if s.nil?
@@ -19,5 +21,23 @@ names.each do |name|
     Episode.pull_episodes(s) if s
   rescue Errno::ECONNRESET => e
     retry
+  end
+end
+
+if(MULTITHREADED)
+  threads = []
+  10.times do |i|
+    threads[i] = Thread.new {
+      10.times do |j|
+        ActiveRecord::Base.connection_pool.with_connection do
+          pull_name(names[10*i + j])
+        end
+      end
+    }
+  end
+  threads.each do |t| t.join end
+else
+  names.each do |name|
+    pull_name(name)
   end
 end
