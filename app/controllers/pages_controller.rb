a26d8195fd9b628e89b5f974543dc451e0fc4472
Leo <github.com/lmartel>
2013-07-25 01:37:47 -0700

    started homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
   def index
-    if current_user.nil?
-      redirect_to shows_path
-    else
-      redirect_to track_path
-    end
+    render "index"
   end
 end
